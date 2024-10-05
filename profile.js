2fcb0ab1b793e44ecf75e50f91f60452acd93694
nada-ahmedd <nada20-02188@student.eelu.edu.eg>
2024-10-05 07:00:57 +0300

    Delete profile.js

--- profile.js
+++
@@ -1,6 +0,0 @@
-
-hamburger = document.querySelector(".header__hamburger");
-hamburger.onclick = function () {
-    navBar = document.querySelector(".header__nav");
-    navBar.classList.toggle("active")
-}
